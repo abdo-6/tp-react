004f82699d718223bb207f26ed8aaa46c69dd9a3
abdo-6 <85464795+abdo-6@users.noreply.github.com>
2022-05-14 09:56:44 +0100

    Add files via upload

---
+++ src/components/multiStep.js
@@ -0,0 +1,17 @@
+import react from 'react';
+import { About, Terms, ConsentForm } from './steps/steps';
+import StepZilla from "react-stepzilla";
+const steps =
+    [
+        { name: 'About', component: <About /> },
+        { name: 'Terms', component: <Terms /> },
+        { name: 'Consent', component: <ConsentForm /> },
+    ]
+const MultiSteps = () => {
+    return (
+        <div className='step-progress'>
+            <StepZilla steps={steps} />
+        </div>
+    );
+}
+export default MultiSteps;
---
+++ src/steps/steps.js
@@ -0,0 +1,4 @@
+import About from './About';
+import Terms from './Terms';
+import ConsentForm from './ConsentForm';
+export {About, Terms, ConsentForm};
